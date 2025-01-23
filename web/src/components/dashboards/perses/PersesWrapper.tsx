00cfb6e8c098e5fd96d295ff2fb485dc63de00c6
PeterYurkovich <pyurkovi@redhat.com>
2025-01-23 09:24:16 -0500

    First pass on perses wrapper

---
+++ web/src/components/dashboards/perses/PersesWrapper.tsx
@@ -0,0 +1,224 @@
+import React, { useMemo } from 'react';
+import {
+  ChartsProvider,
+  generateChartsTheme,
+  getTheme,
+  PersesChartsTheme,
+  typography,
+} from '@perses-dev/components';
+import { ThemeProvider } from '@mui/material';
+import {
+  DataQueriesProvider,
+  dynamicImportPluginLoader,
+  PluginModuleResource,
+  PluginRegistry,
+  TimeRangeProvider,
+  useDataQueries,
+} from '@perses-dev/plugin-system';
+import {
+  DatasourceResource,
+  Definition,
+  DurationString,
+  GlobalDatasourceResource,
+  TimeRangeValue,
+  UnknownSpec,
+} from '@perses-dev/core';
+import panelsResource from '@perses-dev/panels-plugin/plugin.json';
+import prometheusResource from '@perses-dev/prometheus-plugin/plugin.json';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DatasourceApi, DatasourceStoreProvider, VariableProvider } from '@perses-dev/dashboards';
+import { ChartThemeColor, getThemeColors } from '@patternfly/react-charts';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorAlert from 'src/components/console/console-shared/alerts/error';
+import { LoadingInline } from 'src/components/console/utils/status-box';
+import { usePatternFlyTheme } from './usePatternflyTheme';
+
+class DatasourceApiImpl implements DatasourceApi {
+  constructor(public proxyDatasource: GlobalDatasourceResource) {}
+
+  getDatasource(): Promise<DatasourceResource | undefined> {
+    return Promise.resolve(undefined);
+  }
+  getGlobalDatasource(): Promise<GlobalDatasourceResource | undefined> {
+    return Promise.resolve(this.proxyDatasource);
+  }
+  listDatasources(): Promise<DatasourceResource[]> {
+    return Promise.resolve([]);
+  }
+  listGlobalDatasources(): Promise<GlobalDatasourceResource[]> {
+    return Promise.resolve([this.proxyDatasource]);
+  }
+  buildProxyUrl(): string {
+    return '/';
+  }
+}
+
+// Override eChart defaults with PatternFly colors.
+const patternflyBlue300 = '#2b9af3';
+const patternflyBlue400 = '#0066cc';
+const patternflyBlue500 = '#004080';
+const patternflyBlue600 = '#002952';
+const defaultPaletteColors = [patternflyBlue400, patternflyBlue500, patternflyBlue600];
+
+const patternflyChartsMultiUnorderedPalette = getThemeColors(
+  ChartThemeColor.multiUnordered,
+).chart.colorScale.flatMap((cssColor) => {
+  // colors are stored as 'var(--pf-chart-theme--multi-color-unordered--ColorScale--3400, #73c5c5)'
+  // need to extract the hex value, because fillStyle() of <canvas> does not support CSS vars
+  const match = cssColor.match(/#[a-fA-F0-9]+/);
+  return match ? [match[0]] : [];
+});
+
+// PluginRegistry configuration to allow access to
+// visualization panels/charts (@perses-dev/panels-plugin)
+// and data handlers for prometheus (@perses-dev/prometheus-plugin).
+const pluginLoader = dynamicImportPluginLoader([
+  {
+    resource: panelsResource as PluginModuleResource,
+    importPlugin: () => import('@perses-dev/panels-plugin'),
+  },
+  {
+    resource: prometheusResource as PluginModuleResource,
+    importPlugin: () => import('@perses-dev/prometheus-plugin'),
+  },
+]);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0,
+    },
+  },
+});
+
+interface PersesWrapperProps {
+  children?: React.ReactNode;
+}
+
+export function PersesWrapper({ children }: PersesWrapperProps) {
+  const { theme } = usePatternFlyTheme();
+
+  const muiTheme = getTheme(theme, {
+    typography: {
+      ...typography,
+      fontFamily: 'var(--pf-v5-global--FontFamily--text)',
+    },
+    shape: {
+      borderRadius: 0,
+    },
+  });
+
+  const chartsTheme: PersesChartsTheme = generateChartsTheme(muiTheme, {
+    echartsTheme: {
+      color: patternflyChartsMultiUnorderedPalette,
+    },
+    thresholds: {
+      defaultColor: patternflyBlue300,
+      palette: defaultPaletteColors,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={muiTheme}>
+      <ChartsProvider chartsTheme={chartsTheme}>
+        <PluginRegistry pluginLoader={pluginLoader}>
+          <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        </PluginRegistry>
+      </ChartsProvider>
+    </ThemeProvider>
+  );
+}
+
+interface PersesDashboardWrapperProps {
+  timeRange?: TimeRangeValue;
+  setTimeRange?: (value: TimeRangeValue) => void;
+  children?: React.ReactNode;
+}
+
+export function PersesDashboardWrapper({
+  timeRange = { pastDuration: '0s' },
+  setTimeRange,
+  children,
+}: PersesDashboardWrapperProps) {
+  return (
+    <TimeRangeProvider timeRange={timeRange} setTimeRange={setTimeRange}>
+      <VariableProvider>{children}</VariableProvider>
+    </TimeRangeProvider>
+  );
+}
+
+type PrometheusInstance = string;
+
+interface PersesPrometheusDatasourceWrapperProps {
+  prometheus: PrometheusInstance | undefined;
+  queries: Definition<UnknownSpec>[];
+  duration?: DurationString;
+  children?: React.ReactNode;
+}
+
+export function PersesPrometheusDatasourceWrapper({
+  prometheus,
+  queries,
+  children,
+}: PersesPrometheusDatasourceWrapperProps) {
+  const datasourceApi = useMemo(() => {
+    const proxyDatasource: GlobalDatasourceResource = {
+      kind: 'GlobalDatasource',
+      metadata: { name: 'PrometheusProxy' },
+      spec: {
+        default: true,
+        plugin: {
+          kind: 'PrometheusDatasource',
+          spec: {
+            directUrl: prometheus,
+          },
+        },
+      },
+    };
+    return new DatasourceApiImpl(proxyDatasource);
+  }, [prometheus]);
+
+  return (
+    <DatasourceStoreProvider datasourceApi={datasourceApi}>
+      <DataQueriesProvider definitions={queries}>{children}</DataQueriesProvider>
+    </DatasourceStoreProvider>
+  );
+}
+
+interface TimeSeriesPanelWrapperProps {
+  noResults?: React.ReactNode;
+  children?: React.ReactNode;
+}
+
+/**
+ * PrometheusQueryPanelWrapper intercepts the promql query status and displays PatternFly
+ * native empty and loading states
+ * instead of the Material UI empty and loading states used by Perses.
+ */
+export function TimeseriesQueryPanelWrapper({ noResults, children }: TimeSeriesPanelWrapperProps) {
+  const { isFetching, isLoading, queryResults } = useDataQueries('TimeSeriesQuery');
+
+  if (isLoading || isFetching) {
+    return <LoadingInline />;
+  }
+
+  const queryError = queryResults.find((d) => d.error);
+  if (queryError) {
+    return <ErrorAlert error={queryError.error as Error} />;
+  }
+
+  const dataFound = queryResults.some(
+    (timeSeriesData) =>
+      (timeSeriesData.data?.series ?? []).length > 0 || timeSeriesData.data?.series,
+  );
+  if (!dataFound && noResults) {
+    return <>{noResults}</>;
+  }
+
+  return (
+    <ErrorBoundary FallbackComponent={ErrorAlert} resetKeys={[]}>
+      {children}
+    </ErrorBoundary>
+  );
+}
